6e1c25c75c17d5e9b17e91768d192a6462f3ca1b
vanipriya98 <vanipriya192001@gmail.com>
2023-11-13 17:59:35 +0530

    documents & error

--- src/app/common/components/documents/documents.component.ts
+++ src/app/common/components/documents/documents.component.ts
@@ -14,16 +14,25 @@ export class DocumentsComponent {
 
   // constructor(private pdfService: PdfService) { }
 
-  // loadPdf(pdfName: string): void {
-  //   this.selectedPdf = pdfName;
-  //   this.pdfService.getPdfUrl(pdfName).subscribe(data => {
-  //     const blob = new Blob([data], { type: 'application/pdf' });
-  //     this.pdfUrl = URL.createObjectURL(blob);
-  //   });
-  // }
-  pdfFilePath = "https://www.iitk.ac.in/esc101/share/downloads/javanotes5.pdf";
+  
 
-  openFile() {
-    window.open(this.pdfFilePath, '_blank');
+
+  pdfFiles = [
+    
+    "https://www.uncfsu.edu/assets/Documents/College%20of%20Business%20and%20Economics/legal.pdf",
+    "https://medicine.hofstra.edu/pdf/faculty/facdev/facdev_sixlaws_oflearning.pdf",
+    "https://openyls.law.yale.edu/bitstream/handle/20.500.13051/178/Learning_the_Law_catalog__reduced.pdf?sequence=2&isAllowed=y",
+ "https://www.lawspice.com/webresources/webworld/pdf/Futura%20BK%20BT.pdf",
+ "https://www.uncfsu.edu/assets/Documents/College%20of%20Business%20and%20Economics/legal.pdf",
+    "https://medicine.hofstra.edu/pdf/faculty/facdev/facdev_sixlaws_oflearning.pdf",
+    "https://openyls.law.yale.edu/bitstream/handle/20.500.13051/178/Learning_the_Law_catalog__reduced.pdf?sequence=2&isAllowed=y",
+ "https://www.lawspice.com/webresources/webworld/pdf/Futura%20BK%20BT.pdf",
+ "https://www.law.berkeley.edu/wp-content/uploads/2017/11/CommonLawCivilLawTraditions.pdf",
+
+ 
+ 
+  ];
+  openFile(pdfUrl: string) {
+    window.open(pdfUrl, '_blank');
   }
 }
