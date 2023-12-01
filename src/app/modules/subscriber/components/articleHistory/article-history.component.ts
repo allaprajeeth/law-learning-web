c25c2c86b728110a7223fe1f28cc5aa94505dbeb
uppalatirumalesh <uppalatirumalesh@gmail.com>
2023-12-01 13:07:52 +0530

    articleHistorychanges

--- src/app/modules/subscriber/components/articleHistory/article-history.component.ts
+++ src/app/modules/subscriber/components/articleHistory/article-history.component.ts
@@ -7,74 +7,65 @@ import { Component } from '@angular/core';
 })
 
 export class ArticleHistoryComponent {
-  articles = [
+
+  publishedArticles = [
     {
       name: 'The Impact of Technology on Patent Law',
-      status: 'Approved',
       submissionDate: '07-07-2023',
       publishedDate: '10-08-2023',
     },
     {
-      name: 'Law School Applications',
-      status: 'Under Review',
-      submissionDate: '14-09-2023',
-    },
-    {
-      name: 'Immigration Laws and Global Migration Trends',
-      status: 'Commented',
-      submissionDate: '19-10-2023',
-    },
-    {
-      name: 'Legal Ethics in the Legal Profession',
-      status: 'Under Review',
-      submissionDate: '26-09-2023',
-    },
-    {
       name: 'Understanding Intellectual Property Law',
-      status: 'Approved',
       submissionDate: '15-07-2023',
       publishedDate: '17-08-2023',
     },
     {
-      name: 'Landmark Cases in Criminal Law',
-      status: 'Under Review',
-      submissionDate: '03-07-2023',
-    },
-    {
-      name: 'Privacy Laws in the Digital Age',
-      status: 'Commented',
-      submissionDate: '21-07-2023',
-    },
-    {
-      name: 'The Evolution of Constitutional Law',
-      status: 'Under Review',
-      submissionDate: '29-10-2023',
-    },
-    {
       name: 'Challenges in International Human Rights Law',
-      status: 'Approved',
       submissionDate: '11-09-2023',
       publishedDate: '18-09-2023',
     },
     {
+      name: 'Environmental Law and Sustainable Development',
+      submissionDate: '20-07-2023',
+      publishedDate: '28-09-2023',
+    }
+  ];
+
+  underReviewArticles = [
+    {
+      name: 'Law School Applications',
+      submissionDate: '14-09-2023',
+    },
+    {
+      name: 'Landmark Cases in Criminal Law',
+      submissionDate: '03-07-2023',
+    },
+    {
+      name: 'Privacy Laws in the Digital Age',
+      submissionDate: '21-07-2023',
+    },
+    {
+      name: 'The Evolution of Constitutional Law',
+      submissionDate: '29-10-2023',
+    }
+  ];
+
+  commentedArticles = [
+    {
+      name: 'Immigration Laws and Global Migration Trends',
+      submissionDate: '19-10-2023',
+    },
+    {
       name: 'Comparative Legal Systems around the World',
-      status: 'Commented',
       submissionDate: '09-08-2023',
     },
     {
-      name: 'Environmental Law and Sustainable Development',
-      status: 'Approved',
-      submissionDate: '20-07-2023',
-      publishedDate: '28-09-2023',
+      name: 'Legal Ethics in the Legal Profession',
+      submissionDate: '26-09-2023',
     },
     {
       name: 'Legal Implications of Artificial Intelligence',
-      status: 'Commented',
       submissionDate: '27-10-2023',
-    },
+    }
   ];
-
-  publishedArticles: any[] = [];
-  underReviewArticles: any[] = [];
-  commentedArticles: any[] = [];
 }
