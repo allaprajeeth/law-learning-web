58a987eb146d013c998837105df47fd2609e3fd7
uppalatirumalesh <uppalatirumalesh@gmail.com>
2023-12-14 19:05:55 +0530

    AddtoCart using NgRx

--- src/app/modules/subscriber/components/course-card/state/cart.actions.ts
+++ src/app/modules/subscriber/components/course-card/state/cart.actions.ts
@@ -1,22 +1,7 @@
-// import { createAction, props } from '@ngrx/store';
-
-// export const addToCart = createAction(
-//   '[Cart] Go to Cart',
-//   props<{ productId: string }>()
-// );
-
-// export const removeFromCart = createAction(
-//   '[Cart] Add to Cart',
-//   props<{ productId: string }>()
-// );
-
-
-
 import { createAction, props } from '@ngrx/store';
 
 export const addToCart = createAction('[Cart] Add to Cart', props<{ product: any }>());
 export const removeFromCart = createAction('[Cart] Remove from Cart', props<{ productId: string }>());
 export const setGoToCartButtonText = createAction('[Cart] Set Go to Cart Button Text');
 export const setAddToCartButtonText = createAction('[Cart] Set Add to Cart Button Text');
-
-
+export const clearCart = createAction('[Cart] Clear Cart');
---
+++ src/app/modules/subscriber/components/course-card/state/cart.effects.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
+import * as CartActions from './cart.actions';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class CartEffects {
+
+    addToCartRedirect$ = createEffect(() =>
+    this.actions$.pipe(
+    ofType(CartActions.addToCart),
+    tap(() => this.router.navigate(['/cart']))
+   ), { dispatch: false });
+
+  constructor(private actions$: Actions, private router: Router) {}
+}
