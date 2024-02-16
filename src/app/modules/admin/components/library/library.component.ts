7919260b64f3f3b57f5f046c3c542e3fb13b9106
pushpalathashivarathri <pushpalathashivarathri559@gmail.com>
2024-02-16 14:45:01 +0530

    library form

--- src/app/modules/admin/components/library/library.component.ts
+++ src/app/modules/admin/components/library/library.component.ts
@@ -8,26 +8,38 @@ import { IUploadedFile } from 'src/app/shared-module/components/file-upload/file
   styleUrls: ['./library.component.scss']
 })
 export class AdminLibraryComponent {
+  showSuccessMessage: boolean = false;
 
-  form: FormGroup = this.fb.group({});
-  uploadedFiles: Array<IUploadedFile> = []
+  // form: FormGroup = this.fb.group({});
+  // uploadedFiles: Array<IUploadedFile> = []
 
-  constructor(private fb: FormBuilder) {
-    this.createForm();
-  }
-  createForm() {
-    this.form = this.fb.group({
-      name: ['', Validators.required],
-      address: ['', Validators.required]
-    });
-  }
+  // constructor(private fb: FormBuilder) {
+  //   this.createForm();
+  // }
+  // createForm() {
+  //   this.form = this.fb.group({
+  //     name: ['', Validators.required],
+  //     address: ['', Validators.required]
+  //   });
+  // }
 
-  handleUploadedFiles(file: IUploadedFile): void {
-    console.log(file)
-    this.uploadedFiles.push(file);
-  }
+  // handleUploadedFiles(file: IUploadedFile): void {
+  //   console.log(file)
+  //   this.uploadedFiles.push(file);
+  // }
 
   onSubmit() {
-    
+    this.showSuccessMessage = true;
+    console.log('Form submitted:');
   }
+
+  onFileChange(event: any) {
+  
+    // const file = event.target.files[0];
+    // console.log('Selected file:', file);
+  }
+  goBack() {
+    this.showSuccessMessage = false;
+  }
+  
 }
