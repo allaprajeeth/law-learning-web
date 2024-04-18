2524bd49ff73e526695a6aa8064f6085ad925c4b
Glokesh455 <glokesh455a@gmail.com>
2024-04-18 21:02:25 +0530

    PUBLISHING CARNER

--- src/app/shared-module/components/publishingcorner/publishingcorner.component.ts
+++ src/app/shared-module/components/publishingcorner/publishingcorner.component.ts
@@ -17,7 +17,6 @@ export class PublishingcornerComponent implements OnInit {
   isVisible: boolean = false;
 
   userRole: string | undefined
-
   constructor(
     private fetcharticleService: FetcharticlesService,
     private route: ActivatedRoute,
@@ -54,6 +53,9 @@ export class PublishingcornerComponent implements OnInit {
   searchArticles(): void {
     this.filteredArticles = this.articles.filter((article) =>
       article.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    ||
+    (article.author && article.author.toLowerCase().includes(this.searchTerm.toLowerCase()))
+
     );
   }
   navigateToArticleDetail(articleId: number): void {
@@ -89,6 +91,8 @@ export class PublishingcornerComponent implements OnInit {
       this.router.navigateByUrl('/subscriber/articleform');
     } else if (currentUrl.includes('instructor/article')) {
       this.router.navigateByUrl('/instructor/articleform');
+    }else{
+      this.router.navigate(['/login']);
     }
   }
   matchesBaseUrl(): any {
