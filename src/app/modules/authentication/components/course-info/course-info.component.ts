9c6704f66a00e967b6a35b1ccd7ce865573d2f8c
pushpalathashivarathri <pushpalathashivarathri559@gmail.com>
2024-04-22 13:06:35 +0530

    course submitted for review

--- src/app/modules/authentication/components/course-info/course-info.component.ts
+++ src/app/modules/authentication/components/course-info/course-info.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Course } from 'src/app/common/models/course.model';
 
 @Component({
   selector: 'app-course-info',
   templateUrl: './course-info.component.html',
   styleUrls: ['./course-info.component.scss']
 })
-export class CourseInfoComponent {
+export class CourseInfoComponent implements OnInit {
 
+  course: Course | null = null;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation && navigation.extras && navigation.extras.state) {
+      this.course = navigation.extras.state['course'];
+    }
+  }
 }
+
