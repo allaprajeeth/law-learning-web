f7a9cf6be46819b6a39ed065caa76b51f97c5912
Glokesh455 <glokesh455a@gmail.com>
2024-04-26 18:15:05 +0530

    UI issues

--- src/app/common/components/profile/profile.component.ts
+++ src/app/common/components/profile/profile.component.ts
@@ -13,15 +13,21 @@ export class ProfileComponent implements OnInit {
   selectedFile: File | null = null;
   imageUrl: string | null = null;
   selectedFileName: string | null = null;
- 
+  userDetailsSubscription: any;
+  name: string | undefined;
   constructor(private http: HttpClient, private authTokenService: AuthTokenService) { }
  
+
   ngOnInit(): void {
+    this.userDetailsSubscription = this.authTokenService.userDetails$.subscribe(
+      (userDetails: UserModel | null) => {      
+        this.name = userDetails?.name;
+      }
+    );
     const user = this.authTokenService.getUserDetails();
     if (user)
       this.imageUrl = user.imageURL ? endPoints.s3BaseURL + user.imageURL : user.imageURL;
   }
- 
   onFileSelected(event: any): void {
  
     if (event.target.files && event.target.files.length > 0) {
