845572b8a8b6688a65fe108db3a91638b31891ed
ranjith vunnam <rvunnam@sreebtech.com>
2024-05-20 19:25:10 +0530

    Bug fixes.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -24,6 +24,7 @@ export const environment = {
         auth: '/auth',
         secure: '/secure',
         search_courses: '/search-courses',
-        course: '/courses'
+        course: '/courses',
+        unReadNotificationCount: '/profile/notifications/unread/count'
     }
 };
