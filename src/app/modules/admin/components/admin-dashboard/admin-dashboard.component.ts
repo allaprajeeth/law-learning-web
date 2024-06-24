2017a8cb5447285a17a4dcda7091107c78f675ea
Vijaytej19 <vijayteja2000@gmail.com>
2024-06-24 15:23:26 +0530

    added salutation to newly created users

--- src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'; 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { endPoints } from 'src/app/common/constants/endpoints';
@@ -10,10 +10,9 @@ import { NotificationService } from 'src/app/common/services/notification/notifi
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.scss']
+  styleUrls: ['./admin-dashboard.component.scss'],
 })
 export class AdminDashboardComponent implements OnInit {
-
   loginForm!: FormGroup;
   showMessageStatus: boolean = false;
   showcredentialStatus: boolean = true;
@@ -23,42 +22,48 @@ export class AdminDashboardComponent implements OnInit {
   selectedRole: string = 'instructor';
   menuItems: RouteInfo[];
 
-  constructor(private matIconRegistry: MatIconRegistry, 
-    private domSanitizer: DomSanitizer, 
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
     private formBuilder: FormBuilder,
-     private location: Location,
-     private http: HttpClient,
-     private notificationService: NotificationService
-    ) {
+    private location: Location,
+    private http: HttpClient,
+    private notificationService: NotificationService
+  ) {
     // Register Material Icons
     this.matIconRegistry.addSvgIcon(
       'dashboard',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/dashboard.svg')
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'assets/icons/dashboard.svg'
+      )
     );
     this.matIconRegistry.addSvgIcon(
       'person',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/person.svg')
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'assets/icons/person.svg'
+      )
     );
     this.menuItems = [
       { path: '/dashboard', title: 'Dashboard', icon: 'dashboard', class: '' },
-      { path: '/admin/all-profiles', title: 'User Profile', icon: 'person', class: '' },
-    
+      {
+        path: '/admin/all-profiles',
+        title: 'User Profile',
+        icon: 'person',
+        class: '',
+      },
     ];
-
-
   }
   initializeForm() {
     this.loginForm = this.formBuilder.group({
       name: ['', Validators.required],
-      salutation :['',Validators.required],
-      gender :['',Validators.required],
+      salutation: ['', Validators.required],
+      gender: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
       role: ['INSTRUCTOR', Validators.required],
-      jobTitle:['', Validators.required],
-      status:['', Validators.required],
-      about:['', Validators.required],
-
+      jobTitle: ['', Validators.required],
+      status: ['', Validators.required],
+      about: ['', Validators.required],
     });
   }
 
@@ -67,23 +72,29 @@ export class AdminDashboardComponent implements OnInit {
     const apiUrl = baseUrl + `/admin/user-profile`;
     if (this.loginForm.valid) {
       const formData = this.loginForm.value;
-      this.http.post<any>(apiUrl, formData).subscribe(response => {
-        this.showMessageStatus = true;
-        this.showcredentialStatus = false;
-       
-      }, error => {
-        if (error.error.error.type === 'DUPLICATE_RECORD') {
-          this.loginForm.reset()
-          this.notificationService.notify('User EmailId or Mobile number already exists.')
-          console.log('User EmailId or Mobile number already exists.Please Use other credentials');
-        } else {
-          console.error('Error submitting form:', error);
+      this.http.post<any>(apiUrl, formData).subscribe(
+        (response) => {
+          this.showMessageStatus = true;
+          this.showcredentialStatus = false;
+        },
+        (error) => {
+          if (error.error.error.type === 'DUPLICATE_RECORD') {
+            this.loginForm.reset();
+            this.notificationService.notify(
+              'User EmailId or Mobile number already exists.'
+            );
+            console.log(
+              'User EmailId or Mobile number already exists.Please Use other credentials'
+            );
+          } else {
+            console.error('Error submitting form:', error);
+          }
         }
-      });
+      );
+      console.log(formData);
     }
   }
-  
-  
+
   ngOnInit() {
     this.initializeForm();
   }
@@ -104,12 +115,9 @@ export class AdminDashboardComponent implements OnInit {
 
   closePage() {
     // this.closeClicked.emit();
-    location.reload(); 
+    location.reload();
   }
-
-
 }
-
 
 interface RouteInfo {
   path: string;
